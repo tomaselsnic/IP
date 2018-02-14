b2913ac83411722e7ffc981b6d8ccd0097cdfa39
Tomáš Elsnic <tomaselsnic@seznam.cz>
2018-02-14 10:14:39 +0100

    jejednocotamnapises

---
+++ app/presenters/HomepagePresenter.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+
+
+class HomepagePresenter extends Nette\Application\UI\Presenter
+{
+	public function renderDefault()
+	{
+		$this->template->msg="nazdars karlos";
+	}
+}
